cdfc59f7026c369dc73e846938f1acadce4f4e21
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-02-10 18:54:44 +0500

    php | assignment_one

---
+++ assignment/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment</title>
+</head>
+<body>
+    <?php
+
+    // 1: print 1 to 10 and 10 to 1
+    echo "<h3>Print 1 to 10</h3>";
+    for( $i = 1 ; $i <= 10 ; $i++){
+        echo "$i <br>";
+    }
+    echo "<h3>Print 10 to 1</h3>";
+    for( $i = 10 ; $i > 0 ; $i--){
+        echo "$i <br>";
+    }
+    /**
+     * print this
+     *  *
+     *  **
+     *  ***
+     *  ****
+     */
+    echo "<h3>Pattern 1:  </h3>";
+    for ($i=1; $i < 5 ; $i++) { 
+        for ($j=1; $j <= $i ; $j++) { 
+            echo "*";
+        }
+        echo "<br>";
+    }
+    /**
+     * print this
+     *     *
+     *    * *
+     *   * * *
+     *  * * * *
+     */
+    echo "<h3>Pattern 2:  </h3>";
+    for ($i=1; $i < 5 ; $i++) { 
+        for ($k=5; $k > $i ; $k--) { 
+            echo "&nbsp;";
+        }
+        for ($j=1; $j <= $i ; $j++) { 
+            echo "* ";
+        }
+        echo "<br>";
+    }
+    /**
+     * print this
+     *  * * * *
+     *   * * *
+     *    * *
+     *     *
+     */
+    echo "<h3>Pattern 3:  </h3>";
+    for($i = 1; $i < 5; $i++){
+        for($k = 1; $k <= $i ; $k++){
+            echo "&nbsp";
+        }
+        for ($j= 5; $j > $i ; $j--) { 
+            echo "* ";
+        }
+        echo "<br>";
+    }
+    /**
+     *  array
+     */
+    $arr = [8,6,10,2];
+
+    //  find max value in an array through function
+    echo "<h3>Max value in array:  </h3>";
+    function maxValue($x = array()){
+        $fake = $x[0];
+        for ($i=0; $i < count($x); $i++) { 
+            if ($x[$i] > $fake) {
+                $fake = $x[$i];
+            }
+        }
+        echo "max : $fake";
+
+    }
+    maxValue($arr);
+    //  find min value in an array through function
+    echo "<h3>Min value in array:  </h3>";
+    $arr = [8,6,10,2];
+    function minValue($x = array()){
+        $fake = $x[0];
+        for ($i=0; $i < count($x); $i++) { 
+            if ($x[$i] < $fake) {
+                $fake = $x[$i];
+            }
+        }
+        echo "min : $fake";
+
+    }
+    minValue($arr);
+    // sorting an array
+    echo "<h3>Sorting an array:  </h3>";
+    $sortArr = [5,4,3,2,1,0,-4,-2];
+    function sortingArr($arr = array()){
+        $el = 0;
+        for ($i=0; $i < count($arr); $i++) { 
+            for ($j=0; $j < count($arr) ; $j++) { 
+              $check = $i + 1;
+              if($check < count($arr)){
+                if ($arr[$j] > $arr[$i + 1]) {
+                    // a = $arr[$j] b= $arr[$i+1]
+                    $el = $arr[$j];
+                    $arr[$j] = $arr[$i + 1];
+                    $arr[$i + 1] = $el;
+                }
+              }
+            }
+        }
+        print_r($arr);
+    }
+    sortingArr($sortArr);
+    // find n number of max value in an array
+    $arr = [8,6,10,2];
+    echo "<h3>find nth number of largest value in an array:</h3>";
+    function maxValue2($x = array(), $key = 0){
+        $k = $key;
+        $fake = $x[0];
+        for ($n=0; $n < $key; $n++) { 
+            # code...
+            for ($i=0; $i < count($x); $i++) { 
+                if ($x[$i] > $fake) {
+                    $fake = $x[$i];
+                }
+
+            }
+            $k = array_search($fake , $x);
+            $box = $x[$k];
+            $a
+
+        }
+        echo "$key number max : $box ";
+
+    }
+    print_r($arr);
+    echo "<br>";
+    maxValue2($arr , 1);
+    
+    ?>
+</body>
+</html>
